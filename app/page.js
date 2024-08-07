b5d92e1d7cd096c9b875a1a4eedf18e7d2b2ed75
Shalom Steinmetz <shalomsteinmetz@gmail.com>
2024-08-07 16:52:15 -0400

    fix

--- app/page.js
+++ app/page.js
@@ -1,10 +1,53 @@
+"use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
-export default function Home() {
+function App() {
+  const [search, setSearch] = useState("");
+  const [user, setUser] = useState(null);
+  const getUser = () => {
+    return Promise.resolve({ id: "1", name: "joe" });
+  };
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+
+    loadUser();
+  }, []);
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome</h1>
+      {user ? <p>Signed in as {user.name}</p> : null}
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+
+      <p>Searches for {search ? search : "..."}</p>
       <Link href="/about">About</Link>
     </div>
   );
 }
+
+function Search({ value, onChange, children }) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input
+        style={{ color: "green" }}
+        id="search"
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default App;
